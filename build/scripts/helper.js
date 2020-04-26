27c41614e460c06a45cb73a4fc03870d97a7738c
Aleks Bekker <alekso.bekker@gmail.com>
2020-04-26 00:33:05 -0700

    Create query string to object converter function

--- build/scripts/helper.js
+++ build/scripts/helper.js
@@ -1,7 +1,29 @@
 
 
-// Helpers for JS DOM manipulation
-// Inspired by Robin Nixon's Book "Learning PhP, MySQL & JavaScript"
+/* Helpers for JS DOM manipulation
+ * Inspired by Robin Nixon's Book "Learning PhP, MySQL & JavaScript"
+ */
 const O = i => typeof i == 'object' ? i : document.getElementById(i);
 const S = i => O(i).style;
 const C = i => document.getElementsByClassName(i);
+
+/* URL Query Parser
+ * Converts the query string of the current URL into an object such that
+ * {FieldName : FieldValue}
+ */
+function parseUrlQueryString() {
+
+    var queries = window.location.search.substring(1).split('&');
+    var query = {};
+
+    for (var i = 0; i < queries.length; i++) {
+
+        var q = queries[i].split('=');
+
+        query[`${q[0]}`] = q[1];
+
+    }
+
+    return query;
+
+}
