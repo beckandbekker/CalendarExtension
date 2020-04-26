415b463003db197c019d71e25453644484430bf5
Aleks Bekker <alekso.bekker@gmail.com>
2020-04-25 23:55:42 -0700

    Create reasonable popup

--- build/popup/popup.js
+++ build/popup/popup.js
@@ -1,13 +1,18 @@
-var rescanTeamsButton = document.getElementById("rescan_teams");
-var scanHomeworkButton = document.getElementById("scan_homework");
+var rescanTeamsButton = O("rescan_teams");
+var scanHomeworkButton = O("scan_homework");
+var displayCalendarButton = O("open_calendar");
 
 Messages.on(Messages.protocols.PONG, function () {
     console.log("Pong");
 });
 
-rescanTeamsButton.onclick = function () {
+rescanTeamsButton.onclick = () => {
     Messages.send(Messages.protocols.REQUEST_TEAMS_RESCAN);
 }
 
-scanHomeworkButton.onclick = function () {
+scanHomeworkButton.onclick = () => {
+}
+
+displayCalendarButton.onclick = () => {
+
 }
